8cc1589a287a6d907fd40c24fb016f40d0c77d49
Subrata Samartha <subrata.samartha2019@gift.edu.in>
2022-10-23 19:09:30 +0530

    all clear function added to the calculator

--- script.js
+++ script.js
@@ -94,3 +94,8 @@ equalsButton.addEventListener("click", (button) => {
   calculator.compute();
   calculator.updateDisplay();
 });
+
+allClearButton.addEventListener("click", (button) => {
+  calculator.clear();
+  calculator.updateDisplay();
+});
